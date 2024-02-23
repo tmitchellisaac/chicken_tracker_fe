47751a8ff3f2a7bde995f82ba6c4177ca1aeebb6
Nash <141100397+s2an@users.noreply.github.com>
2024-02-23 15:27:19 -0500

    test: Flush out AnimalFacade test

--- spec/facades/animal_facade_spec.rb
+++ spec/facades/animal_facade_spec.rb
@@ -12,7 +12,18 @@ RSpec.describe AnimalFacade do
     animal = facade.animal
     
     expect(animal).to be_a(Animal)
-    expect(animal.id).to eq("1")
-    expect(animal.name).to eq("red barn")
+    expect(animal.id).to eq(animal[:data][:id])
+    expect(animal.name).to eq(animal[:attributes][:name])
+    expect(animal.species).to eq(animal[:attributes][:species])
+    expect(animal.birthday).to eq(animal[:attributes][:birthday])
+    expect(animal.color).to eq(animal[:attributes][:color])
+    expect(animal.main_prey).to eq(animal[:attributes][:main_prey])
+    expect(animal.habitat).to eq(animal[:attributes][:habitat])
+    expect(animal.diet).to eq(animal[:attributes][:diet])
+    expect(animal.top_speed).to eq(animal[:attributes][:top_speed])
+    expect(animal.average_clutch_size).to eq(animal[:attributes][:average_clutch_size])
+    expect(animal.lifespan).to eq(animal[:attributes][:lifespan])
+    expect(animal.avg_weight).to eq(animal[:attributes][:avg_weight])
+    expect(animal.lifestyle).to eq(animal[:attributes][:lifestyle])
   end
 end
