def8e7786cd84e357947e69c283dea6c290d780b
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-23 21:30:05 -0700

    heroku again on a whim

--- app/services/animal_service.rb
+++ app/services/animal_service.rb
@@ -1,8 +1,14 @@
 class AnimalService
+
   def conn
-    Faraday.new(url: "http://localhost:5000") do |faraday|
+    Faraday.new(url: "https://hidden-sands-71693-380133048218.herokuapp.com") do |faraday|
     end
   end
+
+  # def conn
+  #   Faraday.new(url: "http://localhost:5000") do |faraday|
+  #   end
+  # end
 
   def get_url(url)
     response = conn.get(url)
