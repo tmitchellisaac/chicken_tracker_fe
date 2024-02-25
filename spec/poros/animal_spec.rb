391ec8a071481059ee6c97c37d2d483d63d31837
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-25 15:18:22 -0700

    Add all attributes to Animal poro spec/test

--- spec/poros/animal_spec.rb
+++ spec/poros/animal_spec.rb
@@ -3,7 +3,25 @@ require_relative "../../app/poros/animal" # <-- I don't know why this is needed
 
 RSpec.describe Animal do
   it "exists" do
-    attr = { id: 1, attributes: { name: "McClucky", species: "Chicken", birthday: "3/3/2024", color: "brown" } }
+    attr = {:id=>"1",
+    :type=>"animal",
+    :attributes=>
+     {:shelter_id=>1,
+      :name=>"Tom",
+      :species=>"chicken",
+      :birthday=>"2023-03-03",
+      :color=>"orange",
+      :main_prey=>"Seeds, Fruit, Insects, Berries",
+      :habitat=>"Open woodland and sheltered grassland",
+      :diet=>"Omnivore",
+      :skin_type=>"Feathers",
+      :top_speed=>"6 mph",
+      :avg_litter=>"2",
+      :lifespan=>"2 - 4 years",
+      :weight=>"1kg - 3kg (2.2lbs - 6.6lbs)",
+      :lifestyle=>"Flock",
+      :fav_food=>"Seeds"},
+    :relationships=>{:shelter=>{:data=>{:id=>"1", :type=>"shelter"}}}}
     animal = Animal.new(attr)
 
     expect(animal).to be_a(Animal)
