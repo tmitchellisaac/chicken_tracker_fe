fa145ee7748ab9245af87091ca0c784588074493
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-25 15:16:49 -0700

    Add/complete test for animal show page with stub

--- spec/features/animals/show_animal_spec.rb
+++ spec/features/animals/show_animal_spec.rb
@@ -7,14 +7,24 @@ RSpec.describe "Animal Show Page", type: :feature do
   # I see "Animal Show"
     it "displays the page title" do
       # stub not created yet!!!
-      json_response = File.read('spec/fixtures/animal_1.json')
-      stub_request(:get, "http://localhost:3000/api/v1/animals/1").
-      to_return(status: 200, body: json_response, headers: {})
-      
-      visit "/animals/1"
+      json_response = File.read('spec/fixtures/animals_show.json')
+      # stub_request(:get, "http://localhost:3000/api/v1/animals/1").
+      # to_return(status: 200, body: json_response, headers: {})
 
-      expect(current_path).to eq("/animals/1")
-      expect(page).to have_content("Animal Show")
+      stub_request(:get, "http://localhost:5000/api/v1/animals/1").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.9.0'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+
+      animal = Animal.new(JSON.parse(json_response, symbolize_names: true)[:data].first)
+      
+      visit "/shelters/#{animal.shelter_id}/animals/#{animal.id}"
+      expect(current_path).to eq("/shelters/#{animal.shelter_id}/animals/#{animal.id}")
+      expect(page).to have_content("Animal Show Page")
     end
   end
 end
