602c7af48fffc9571c03e87189c46f5417e427ba
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-25 15:14:52 -0700

    Add stub/fixture for login_in feature test

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Logging In" do
   before(:each) do
-    @user = User.create(email: "test@test.com", password: "test")
+    @user = User.create(email: "test@test.com", password: "test", id: 77)
   end
 
   it "can log in with valid credentials" do
+    json_response = File.read("spec/fixtures/shelters_index.json")
+    stub_request(:get, "http://localhost:5000/api/v1/shelters?user_id=77").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.9.0'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+
     visit log_in_path
 
     fill_in :email, with: @user.email
