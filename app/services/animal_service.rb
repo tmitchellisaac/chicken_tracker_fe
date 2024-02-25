9d9e7005388bdb92bf776cc2db21f40b7b665f22
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-25 15:12:19 -0700

    changed faraday to localhost:5000, added create animal post method in animal service

--- app/services/animal_service.rb
+++ app/services/animal_service.rb
@@ -2,7 +2,7 @@ class AnimalService
 
 
   def conn
-    Faraday.new(url: "http://localhost:3000") do |faraday|
+    Faraday.new(url: "http://localhost:5000") do |faraday|
     end
   end
 
@@ -13,5 +13,18 @@ class AnimalService
 
   def get_animal(animal_id)
     get_url("api/v1/animals/#{animal_id}")
+  end
+
+  def post_url(url)
+    response = conn.post(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def create_animal(new_animal_data)
+    response = conn.post("/api/v1/shelters/#{new_animal_data[:shelter_id]}/animals") do |req|
+      req.headers['CONTENT_TYPE" => "application/json']
+      req.body = JSON.generate(animal: new_animal_data)
+    end
+   JSON.parse(response.body, symbolize_names: true)
   end
 end
