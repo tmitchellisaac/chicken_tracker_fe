9d923ec8dbfe39fb67283ccf047760a1a524a650
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-27 14:05:45 -0700

    Add destroy action and deleted prys

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -32,8 +32,6 @@ class AnimalsController < ApplicationController
   end
 
   def update
-    #create a patch request to send to the BE API that includes the updated information
-    # require 'pry'; binding.pry
     updated_animal_data = ({
       "shelter_id": params[:shelter_id].to_i,
       "name": params[:name],
@@ -42,17 +40,25 @@ class AnimalsController < ApplicationController
       "birthday": params[:birthday]
   })
     facade = AnimalFacade.new(params)
-    # updated_animal = facade.update_animal(updated_animal_data)
-
     if facade.update_animal(updated_animal_data).attributes_match?(updated_animal_data)
       flash[:alert] = "You've successfully updated your animal!"
       redirect_to "/shelters/#{params[:shelter_id]}/animals/#{params[:id]}"
     else
       flash[:alert] = "Sorry, something went wrong"
-      render :edit
+      redirect_to "/shelters/#{params[:shelter_id]}/animals/#{params[:id]}"
+
     end
   end
 
+  def destroy
+    if AnimalFacade.new(params).delete_animal == 204
+      flash[:alert] = "Animal successfully deleted"
+      redirect_to "/shelters/#{params[:shelter_id]}"
+    else
+      flash[:alert] = "Something went wrong please try again"
+      redirect_to "/shelters/#{params[:shelter_id]}/animals/#{params[:id]}"
+    end
+  end
 
   private
 
