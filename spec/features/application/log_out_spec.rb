64bab3ff72e36c89f8bfd0c0ce73f3e1a91a19b8
Nash <141100397+s2an@users.noreply.github.com>
2024-02-27 16:44:08 -0500

    test: Add test for log_out link and back to home button

---
+++ spec/features/application/log_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Log_out Page" do
+  it "has a link to go back to home ('/')" do
+    user = User.create!(email: "test@test.com", password:"password123", id: 1)
+    visit "/users/1"
+    expect(page).to have_link("Log Out")
+    click_link("Log Out")
+
+    expect(current_path).to eq(log_out_path)
+
+    expect(page).to have_button("Back to Home")
+    click_button("Back to Home")
+
+    expect(current_path).to eq("/")
+  end
+end
