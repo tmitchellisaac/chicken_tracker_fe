caab7325b7735fd5e059a603a8326ff5d593a823
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-28 15:13:14 -0700

    heroku oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET']
+  provider :google_oauth2, ENV['271838257306-2od0075jb8bepbd9cqo6tuo9q18etn4a.apps.googleusercontent.com'], ENV['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']
 end
 OmniAuth.config.allowed_request_methods = %i[get]
