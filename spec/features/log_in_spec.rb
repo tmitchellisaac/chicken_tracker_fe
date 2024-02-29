a3b207db4fc1376c96e0eed264724da1cf36799f
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-29 10:18:36 -0700

    fix logout/login redirects in tests

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "Logging In" do
   before(:each) do
     @user = User.create(email: "test@test.com", password: "test", password_confirmation: "test", id: 77)
 
-    json_response = File.read("spec/fixtures/shelters_index.json")
-    stub_request(:get, "http://localhost:5000/api/v1/shelters?user_id=77").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v2.9.0'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
+    # json_response = File.read("spec/fixtures/shelters_index.json")
+    # stub_request(:get, "http://localhost:5000/api/v1/shelters?user_id=77").
+    #      with(
+    #        headers: {
+    #       'Accept'=>'*/*',
+    #       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #       'User-Agent'=>'Faraday v2.9.0'
+    #        }).
+    #      to_return(status: 200, body: json_response, headers: {})
          
     visit log_in_path
   end
