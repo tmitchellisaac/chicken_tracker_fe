e5a61c2d63c89fd87b8fea1a49689f16b063acff
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-29 12:35:51 -0700

    add image of chicken/bee on animal show page based on Animal Type

--- spec/decorators/animal_decorator_spec.rb
+++ spec/decorators/animal_decorator_spec.rb
@@ -4,28 +4,44 @@ RSpec.describe AnimalDecorator do
   describe '#formatted_birthday' do
     it 'formats the birthday attribute' do
 
-    animal = Animal.new({
-      "id": "1",
-      "type": "animal",
-      "attributes": {
-          "shelter_id": 1,
-          "name": "Tom",
-          "species": "chicken",
-          "birthday": "2023-03-03",
-      },
-      "relationships": {
-        "shelter": {
-          "data": {
-            "id": "1",
-            "type": "shelter"
-          }
-        }
-      }
-    })
+      animal = instance_double("Animal", birthday: Date.new(2023, 3, 3))
+      decorated_animal = AnimalDecorator.new(animal)
 
-    decorated_animal = described_class.new(animal)
-
-    expect(decorated_animal.formatted_birthday).to eq('March 03, 2023')
+      expect(decorated_animal.formatted_birthday).to eq('March 03, 2023')
+      end
     end
   end
+
+  describe "#animal_image" do
+    describe "[happy path]" do
+      it "returns an image tag for a chicken" do
+        animal = instance_double("Animal", species: "Chicken")
+        decorated_animal = AnimalDecorator.new(animal)
+
+        expect(decorated_animal.animal_image).to eq(ActionController::Base.helpers.image_tag("chicken.jpeg", alt: "Chicken", size: "50x50"))
+      end
+
+      it "returns an image tag for a bee" do
+        animal = instance_double("Animal", species: "Bee")
+        decorated_animal = AnimalDecorator.new(animal)
+
+        expect(decorated_animal.animal_image).to eq(ActionController::Base.helpers.image_tag("bee.jpeg", alt: "Bee", size: "50x50"))
+      end
+    end
+
+    describe "[sad path]" do
+      it "returns nil if species is not chicken" do
+        animal = instance_double("Animal", species: "Not Chicken")
+        decorated_animal = AnimalDecorator.new(animal)
+
+        expect(decorated_animal.animal_image).to be_nil
+      end
+
+      it "returns nil if species is not bee" do
+        animal = instance_double("Animal", species: "Not Bee")
+        decorated_animal = AnimalDecorator.new(animal)
+
+        expect(decorated_animal.animal_image).to be_nil
+      end
+    end
 end
