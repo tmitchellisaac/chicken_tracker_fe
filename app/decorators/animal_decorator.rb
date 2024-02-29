40857cc1f6796b1bff2df2227c29a4742d922e0b
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-28 21:29:13 -0700

    add chicken and bee images to the animal show page

--- app/decorators/animal_decorator.rb
+++ app/decorators/animal_decorator.rb
@@ -4,4 +4,17 @@ class AnimalDecorator < Draper::Decorator
   def formatted_birthday
     object.birthday.to_date.strftime('%B %d, %Y')
   end
+
+  def chicken_image
+    ActionController::Base.helpers.image_tag("chicken.jpeg", alt: "Chicken", size: "50x50") if object.species == "Chicken"
+  end
+
+  def bee_image
+    ActionController::Base.helpers.image_tag("bee.jpeg", alt: "Bee", size: "50x50") if object.species == "Bee"
+  end
+
+  # def animal_image
+  #   ActionController::Base.helpers.image_tag("chicken.jpeg", alt: "Chicken", size: "50x50") if object.species == "Chicken"
+  #   ActionController::Base.helpers.image_tag("bee.jpeg", alt: "Bee", size: "50x50") if object.species == "Bee"
+  # end
 end
