0df2458e117d807b992cbc54d32cff39736f18f1
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-17 12:06:07 -0700

    fix: Remove pry from Shelter

--- app/poros/shelter.rb
+++ app/poros/shelter.rb
@@ -2,7 +2,6 @@ class Shelter
   attr_reader :id, :name, :user_id
 
   def initialize(attr)
-    require 'pry'; binding.pry
     @id = attr[:id].to_i
     @name = attr[:attributes][:name]
     @user_id = attr[:attributes][:user_id].to_i
