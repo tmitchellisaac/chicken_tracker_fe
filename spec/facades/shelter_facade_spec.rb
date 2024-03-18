e46db4103c547c18d3b9424c7d6eeea1801eeb69
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-17 18:13:33 -0700

    feat: Add extra fixture for testing, add extra testing for num_of_animals

--- spec/facades/shelter_facade_spec.rb
+++ spec/facades/shelter_facade_spec.rb
@@ -41,6 +41,20 @@ RSpec.describe ShelterFacade do
   describe "num_of_animals" do
     it "counts the number of animals in a shelter" do
       expect(@facade.num_of_animals).to eq(5)
+
+      shelters_animals = File.read("spec/fixtures/animals_index_2.json")
+      stub_request(:get, "https://hidden-sands-71693-380133048218.herokuapp.com/api/v1/shelters/1/animals").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.9.0'
+        }).
+      to_return(status: 200, body:  shelters_animals, headers: {})
+
+      facade = ShelterFacade.new(id: 1, name: "red barn", user_id: "1")
+
+      expect(facade.num_of_animals).to eq(3)
     end
   end
 end
