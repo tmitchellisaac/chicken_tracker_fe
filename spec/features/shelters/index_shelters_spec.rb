329621611937e64aa5dceb5d778810d708e8be91
Nash <141100397+s2an@users.noreply.github.com>
2024-03-23 13:43:37 -0400

    Add heroku stub request

--- spec/features/shelters/index_shelters_spec.rb
+++ spec/features/shelters/index_shelters_spec.rb
@@ -24,12 +24,8 @@ RSpec.describe "Shelter index", type: :feature do
     shelters_index = File.read("spec/fixtures/shelters_index.json")
     
     stub_request(:get, "http://localhost:5000/api/v1/shelters?user_id=1").
-      with(
-        headers: {
-              'Accept'=>'*/*',
-              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'User-Agent'=>'Faraday v2.9.0'
-        }).
+      to_return(status: 200, body: shelters_index, headers: {})
+    stub_request(:get, "https://hidden-sands-71693-380133048218.herokuapp.com/api/v1/shelters?user_id=1").
       to_return(status: 200, body: shelters_index, headers: {})
     
     visit "/users/1"
