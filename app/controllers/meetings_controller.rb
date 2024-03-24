1194a9cc374d9ccaf959abbbc5ce20a4a2410476
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-24 11:06:34 -0700

    feat: Add styling to Event Show

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -22,8 +22,7 @@ class MeetingsController < ApplicationController
   end
 
   def create
-    require 'pry'; binding.pry
-    meeting = SingleDayEvent.create!(meeting_params.merge(user_id: params[:user_id]))
+    meeting = SingleDayEvent.create!(meeting_params)
 
     redirect_to meeting_path(meeting, user_id: meeting.user_id)
   end
@@ -35,7 +34,7 @@ class MeetingsController < ApplicationController
   def update
   meeting = SingleDayEvent.find(params[:id])
 
-    meeting.update!(meeting_params.merge(user_id: params[:user_id]))
+    meeting.update!(meeting_params)
 
     redirect_to meeting_path(meeting, user_id: meeting.user_id)
 
@@ -55,6 +54,18 @@ class MeetingsController < ApplicationController
   private
 
   def meeting_params
-    params.require(:meeting).permit(:name, :notes, :location, :time)
+    meeting_params = params.require(:meeting).permit(:name, :notes, :location, :start_time, :time).merge(user_id: params[:user_id])
+    date_params = format_date_params  # Extract date parameters
+    date = Date.new(*date_params.values.map(&:to_i))  # Convert date parameters to a Date object
+    meeting_params[:start_time] = date  # Assign the Date object back to the meeting_params hash
+    meeting_params
+  end
+
+  def format_date_params
+    date_params = Hash.new
+    date_params[:year] = params[:meeting]["start_time(1i)"].to_i
+    date_params[:month] = params[:meeting]["start_time(2i)"].to_i
+    date_params[:day] = params[:meeting]["start_time(3i)"].to_i
+    date_params
   end
 end
